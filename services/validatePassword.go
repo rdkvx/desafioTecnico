39e0a26177b30644639fad15135d7e61f6a8b0f4
rdkvx <digoftw@hotmail.com>
2022-12-04 02:17:02 -0300

    Finished all validations

--- services/validatePassword.go
+++ services/validatePassword.go
@@ -1,7 +1,6 @@
 package services
 
 import (
-	"fmt"
 	"regexp"
 	"unicode"
 
@@ -15,30 +14,78 @@ func ValidatePassword(bodyRequest model.ValidatePassword) (failedRules []string)
 	for _, rule := range bodyRequest.Rules {
 		switch rule.Rule {
 		case "minSize":
-			hasLower := 0
-
-			for _, r := range bodyRequest.Password {
-				if unicode.IsLower(r) {
-					hasLower++
-				}
-			}
-
-			if hasLower < rule.Value {
+			if len(bodyRequest.Password) < rule.Value {
 				failedRules = append(failedRules, "minSize")
 			}
 
-		case "minSpecialChars":
-			hasSpecialChars := 0
-			re := regexp.MustCompile(`[^a-zA-Z0-9]+`)
-			
-			submatchall := re.FindAllString(bodyRequest.Password, -1)
-			for _, element := range submatchall{
-				fmt.Println(element)
-				hasSpecialChars++
+		case "minLowercase":
+			minLowercaseErr := 0
+
+			for _, r := range bodyRequest.Password {
+				if unicode.IsLower(r) {
+					minLowercaseErr++
+				}
 			}
 
-			if hasSpecialChars < rule.Value{
+			if minLowercaseErr < rule.Value {
+				failedRules = append(failedRules, "minLowercase")
+			}
+
+		case "minUppercase":
+			minUppercaseErr := 0
+
+			for _, r := range bodyRequest.Password {
+				if unicode.IsUpper(r) {
+					minUppercaseErr++
+				}
+			}
+
+			if minUppercaseErr < rule.Value {
+				failedRules = append(failedRules, "minUppercase")
+			}
+
+		case "minDigit":
+			minDigitErr := 0
+
+			re := regexp.MustCompile(`[-]?\d[\d,]*[\.]?[\d{2}]*`)
+			subMatchAll := re.FindAllString(bodyRequest.Password, -1)
+			for range subMatchAll {
+				minDigitErr++
+			}
+
+			if minDigitErr < rule.Value {
+				failedRules = append(failedRules, "minDigit")
+			}
+
+		case "minSpecialChars":
+			specialCharsErr := 0
+			re := regexp.MustCompile(`[^a-zA-Z0-9]+`)
+
+			subMatchAll := re.FindAllString(bodyRequest.Password, -1)
+			for range subMatchAll {
+				specialCharsErr++
+			}
+
+			if specialCharsErr < rule.Value {
 				failedRules = append(failedRules, "minSpecialChars")
+			}
+
+		case "noRepeated":
+			noRepeatedErr := 0
+			var lastChar rune
+			for _, r := range bodyRequest.Password{
+				if r == lastChar{
+					noRepeatedErr++
+				}
+				lastChar = r
+			}
+
+			if noRepeatedErr > 0{
+				noRepeatedErr++
+			}
+
+			if noRepeatedErr > rule.Value{
+				failedRules = append(failedRules, "noRepeated")
 			}
 		}
 
